638791f804af9d3c8df0dc2a0b85e686e6259703
ludwig <ludwigstridh@gmail.com>
2025-02-02 11:46:48 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -22,48 +22,51 @@ const Cart = () => {
 	);
 };
 
-const Product = ({title,author,text}) => {
- 
+const Product = ({ title, author, text }) => {
 	return (
 		<div className="product">
 			<h1>{title}</h1>
 			<h3>{author}</h3>
-			<p>
-				{text}
-			</p>
+			<p>{text}</p>
 			<button>Add to cart</button>
 		</div>
 	);
 };
 
 const App = () => {
-  const info = [
-    {
-      title: "A Study in Scarlet",
-      author: "Arthur Conan Doyle",
-      text: "The first novel introducing Sherlock Holmes and Dr. Watson, as they solve a mysterious murder involving revenge and a long-buried secret."
-    },
-    {
-      title: "The Hound of the Baskervilles",
-      author: "Arthur Conan Doyle",
-      text: "Holmes and Watson investigate the legend of a supernatural hound terrorizing the Baskerville family on the moors of Devonshire."
-    },
-    {
-      title: "The Adventures of Sherlock Holmes",
-      author: "Arthur Conan Doyle",
-      text: "A collection of short stories featuring Holmes solving various intriguing and perplexing cases."
-    },
-    {
-      title: "The Final Problem",
-      author: "Arthur Conan Doyle",
-      text: "Sherlock Holmes faces his greatest adversary, Professor Moriarty, in a deadly confrontation at the Reichenbach Falls."
-    }
-  ];
+	const info = [
+		{
+			title: "A Study in Scarlet",
+			author: "Arthur Conan Doyle",
+			text: "The first novel introducing Sherlock Holmes and Dr. Watson, as they solve a mysterious murder involving revenge and a long-buried secret.",
+		},
+		{
+			title: "The Hound of the Baskervilles",
+			author: "Arthur Conan Doyle",
+			text: "Holmes and Watson investigate the legend of a supernatural hound terrorizing the Baskerville family on the moors of Devonshire.",
+		},
+		{
+			title: "The Adventures of Sherlock Holmes",
+			author: "Arthur Conan Doyle",
+			text: "A collection of short stories featuring Holmes solving various intriguing and perplexing cases.",
+		},
+		{
+			title: "The Final Problem",
+			author: "Arthur Conan Doyle",
+			text: "Sherlock Holmes faces his greatest adversary, Professor Moriarty, in a deadly confrontation at the Reichenbach Falls.",
+		},
+	];
 	return (
 		<div className="parent">
 			<Header />
 			<div className="content">
-				{info.map(inf => (<Product title={inf.title} author={inf.author} text={inf.text}  />))}
+				{info.map((inf) => (
+					<Product
+						title={inf.title}
+						author={inf.author}
+						text={inf.text}
+					/>
+				))}
 			</div>
 		</div>
 	);
